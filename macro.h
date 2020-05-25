49fc0ba23d87db4ee68414d25abdcc9666058fe9
dheeraj poonia <2017pietcsdheeraj035@poornima.org>
2020-05-25 11:41:15 +0530

    Update macro.h

--- macro.h
+++ macro.h
@@ -5,17 +5,26 @@ using namespace std;
 
 //------------------------- syntax shortening ----------------------
 #define ll                  long long int
-#define nl                  cout<<"\n"
+#define ull                 unsigned long long int
+#define el                  cout<<"\n"
 #define np                  nullptr
-#define fastio              ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-#define pb push_back
+#define endl								"\n"
+#define hello              ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define world								int n, sum, res, i, j;
+#define pb 									push_back
+#define mp                  make_pair
+#define vi									vector<int>
+#define vall(vec)						vec.begin(), vec.end()
+#define vitr(cursor)				vector<int>::iterator cursor;
 
 //---------------------- common declaration macro ---------------
 #define si(x)	            scanf("%d",&x)
+#define sf(x)	            scanf("%f",&x)
 #define sl(x)	            scanf("%lld",&x)
 #define ss(s)	            scanf("%s",s)
+#define ph(x)							printf("%s\n", "hello world!")
 #define pi(x)	            printf("%d\n",x)
+#define pf(x)							printf("%f\n",x)
 #define pl(x)	            printf("%lld\n",x)
 #define ps(s)	            printf("%s\n",s)
 
@@ -28,23 +37,21 @@ using namespace std;
 #define whltc(tc)           int tc; si(tc); while(tc--)
 
 //--------------- debugging macro --------------------------
-#define DBmsg(x)            cout<<">"<< x ;
-#define DBerror(x)          cout<<"!error"<< "=" << x << endl;
-#define DBx(x)              cout << #x << "=" << x << endl;
-#define DBx_line(x)         cout<< x <<" ";
-#define DBxy(x, y)          cout << #x << "=" << x << "," << #y << "=" << y << endl;
-#define DBxyz(x,y,z)        cout << #x << "=" << x << ",\t" << #y << "=" << y << ",\t" << #z << "=" << z << endl;
-#define DBxyzw(x,y,z,w)     cout << #x << "=" << x << ",\t" << #y << "=" << y << ",\t" << #z << "=" << z << ",\t" << #w << "=" << w << endl;
+#define px(x)              cout << #x << "=" << x << endl;
+#define pxln(x)         cout<< x <<" ";
+#define pxy(x, y)          cout << #x << "=" << x << "," << #y << "=" << y << endl;
+#define pxyz(x,y,z)        cout << #x << "=" << x << ",\t" << #y << "=" << y << ",\t" << #z << "=" << z << endl;
+#define pxyzw(x,y,z,w)     cout << #x << "=" << x << ",\t" << #y << "=" << y << ",\t" << #z << "=" << z << ",\t" << #w << "=" << w << endl;
 
-#define DBarr(arr,n)        cout<< #arr << "=" ; for(int i=0;i<n;i++) cout<< arr[i] <<" "; cout<< endl;
-#define DBarr_rev(arr, n)   cout<< #arr << "=" ; for(int i=n-1;i>=0;i--) cout<< arr[i] <<" "; cout<< endl;
-#define DBmat(mat,r,c)      cout<< #mat << "=" << endl;  for(int i=0; i<r; i++){ for(int j=0; j<c;j++)   cout<<mat[i][j]<<" ";   cout<< endl; } cout<<endl;
+#define pa(arr,n)        		cout<< #arr << "=" ; for(int i=0;i<n;i++) cout<< arr[i] <<" "; cout<< endl;
+#define pm(mat,r,c)      		cout<< #mat << "=" << endl;  for(int i=0; i<r; i++){ pv(mat[i]) } cout<<endl;
 
-#define DBvec(vec)          cout<< #vec << "=" ; for(auto ele: vec)  cout<< ele <<" ";  cout<< endl;
-#define DBhashmp(map)       cout<< #map << "=" ;  for(auto pair: map) cout<< pair.first<< " " << pair.second << "\n";  cout<< endl;
+#define pv(vec)          		cout<< #vec << "=" ; for(auto ele: vec)  cout<< ele <<" ";  cout<< endl;
+#define pvp(vec)						cout<< #vec << "=\n" ; for(auto v: vec)	cout<< v.first <<":"<< v.second << endl;	 cout<< endl;
+#define phsmp(map)       		cout<< #map << "=" ;  for(auto pair: map) cout<< pair.first<< " " << pair.second << "\n";  cout<< endl;
 
-#define DBsep1()            cout<<"\n_________________________"<<" \n";
-#define DBsep2()            cout<<"-------------------------"<<" \n";
+#define psep1()            cout<<"\n_________________________"<<" \n";
+#define psep2()            cout<<"-------------------------"<<" \n";
 
 //------------------ math macro --------------------------
 #define max(a,b)            ((a)>(b)?(a):(b))
@@ -54,7 +61,9 @@ using namespace std;
 #define prec(x,y)           fixed<<setprecision(y)<<x;
 //prec(ans,6)
 
+
+
 //----------------------- boolean macro -------------------------
 #define                     isKeyExist(hashtbl, key) (hashtbl.find(key)!=hashtbl.end())
-#define stlUb(vec, ele)     vector<int>::iterator ub;
-
+#define lb(vec,key,l,r) 				(lower_bound(vec.begin()+l, vec.end()-r, key) - vec.begin());  
+#define ub(vec,key,l,r) 				(upper_bound(vec.begin()+l, vec.end()-r, key) - vec.begin());
